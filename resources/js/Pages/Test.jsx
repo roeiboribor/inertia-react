ae8aaca6848bfaac080fd8a7d2c68a3f528b03d7
Roei Boribor <roeiboribor@gmail.com>
2023-04-06 10:21:45 +0800

    minor changes

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Head } from "@inertiajs/inertia-react";
+
+const Test = () => {
+    return (
+        <div>
+            <h1>Test Chat</h1>
+        </div>
+    );
+};
+export default Test;
