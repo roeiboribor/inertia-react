017d9d69ed0b2b9cc7adae61c58119d9eb4f2b12
Roei Boribor <roeiboribor@gmail.com>
2023-04-06 10:04:41 +0800

    initialize progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init();
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
